02a0ced5d50e5f5647859269f6447619fb97bd66
dar4ds <amievarodriguez.daniel@gmail.com>
2023-11-05 00:21:28 -0600

    update box use

--- app/logic/auth_spotify.R
+++ app/logic/auth_spotify.R
@@ -1,32 +1,29 @@
-box::use(
-  httr[GET,
-       POST,
-       authenticate,
-       config,
-       content,
-       accept_json],
-  purrr[pluck]
-)
+box::use(httr[GET,
+              POST,
+              authenticate,
+              config,
+              content,
+              accept_json],
+         purrr[pluck])
 
 #' @export
 get_spotify_token <- function(client_id, client_secret) {
-  
   token_response <- POST(
     'https://accounts.spotify.com/api/token',
     accept_json(),
     authenticate(
       Sys.getenv('SPOTIFY_CLIENT_ID'),
       Sys.getenv('SPOTIFY_CLIENT_SECRET')
-      ),
+    ),
     body = list(grant_type = 'client_credentials'),
     encode = 'form',
     config(http_version = 2)
-  ) 
+  )
   
-  token <- token_response |> 
-    content() |> 
+  token <- token_response |>
+    content() |>
     pluck('access_token')
-    
+  
   
   return(token)
 }
