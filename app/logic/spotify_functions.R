69910e4fd2bf68877802d96c9ba8a1a3a672cf76
dar4ds <amievarodriguez.daniel@gmail.com>
2023-11-05 00:21:42 -0600

    spotify functions as utils

--- app/logic/spotify_functions.R
+++ app/logic/spotify_functions.R
@@ -1,13 +1,25 @@
 box::use(
-   httr[GET],
-   purrr,
-   stringr[str_replace, str_replace_all],
-   tibble,
-   tidyr[map_df],
+         stringr[str_replace, str_replace_all],
+         tibble[tibble]
+         )
+
+
+clean_artist_response <- function(search_artist_response_list) {
+   # Clean response
+   artists_found_df <- tibble(
+      artist_name = search_artist_response_list$name,
+      artist_id = search_artist_response_list$id,
+      artist_uri = str_replace(search_artist_response_list$uri, 'spotify:artist:', ''),
+      artist_followers = search_artist_response_list$followers$total,
+      artist_popularity = search_artist_response_list$popularity,
+      artist_genres = list( search_artist_response_list$genres),
+      artist_img = ifelse(length(search_artist_response_list$images) > 0,
+                          search_artist_response_list$images[[1]]$url,
+                          NA) |> 
+         as.character() |> 
+         list()
    )
+   
 
-
-
-
-
-
+   return(artists_found_df)
+}
